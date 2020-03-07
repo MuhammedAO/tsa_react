ebe6b73ce3146e11b826ccb92a0d2aba2fca7bce
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-03-07 23:35:20 +0100

    Component folder created

---
+++ src/components/CountDown.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const CountDown = (props) => {
+    return (
+        <section className="hero is-success is-fullheight">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">
+              Fullheight title
+            </h1>
+            <h2 className="subtitle">
+              Fullheight subtitle
+            </h2>
+          </div>
+        </div>
+      </section>
+    )
+}
+
+export default CountDown
