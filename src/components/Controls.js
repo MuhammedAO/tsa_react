15f381637d6d6f8d94df2078120397cf3e73fdcb
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-03-08 23:01:59 +0100

    control btns

---
+++ src/components/Controls.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Controls = (paused) => {
+    return (
+        <div className="field is-grouped is-grouped-centered">
+        <p className="control">
+          <button className="button is-danger is-outlined is-rounded is-medium" disabled={paused}>
+            Pause
+          </button>
+        </p>
+        <p className="control">
+          <button className="button is-success is-outlined is-rounded is-medium" disabled={!paused}>
+            Resume
+          </button>
+        </p>
+      </div>
+    )
+}
+
+export default Controls
